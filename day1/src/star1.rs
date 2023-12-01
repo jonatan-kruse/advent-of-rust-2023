4977f50645ff9bb8b4d59812d0c505c6a0370a65
Jonatan Kruse <jonatan.kruse@spiideo.com>
2023-12-01 10:22:47 +0100

    Complete day1

--- day1/src/star1.rs
+++ day1/src/star1.rs
@@ -1,7 +1,7 @@
-
 pub fn star1() {
-    let lines = include_str!("i1").lines();
-    lines.for_each(|line|{
-        dbg!(line);
-    })
+    println!("star1: {}", (include_str!("i1")
+        .lines()
+        .map(|l| l.chars().filter(|c| c.is_numeric()).collect::<Vec<char>>())
+        .map(|l| format!("{}{}", l[0], l[l.len() - 1]).parse::<i32>().unwrap())
+        .sum::<i32>()));
 }
--- day1/src/star2.rs
+++ day1/src/star2.rs
@@ -1,7 +1,49 @@
-
 pub fn star2() {
-    let lines = include_str!("i1").lines();
-    lines.for_each(|line| {
-        dbg!(line);
-    })
+    println!(
+        "star2: {}",
+        (include_str!("i1")
+            .lines()
+            .map(str_to_numbs)
+            .map(|l| format!("{}{}", l[0], l[l.len() - 1])
+                .parse::<i32>()
+                .unwrap())
+            .sum::<i32>())
+    );
+}
+
+fn str_to_numbs(str: &str) -> Vec<i32> {
+    let mut temp: Vec<char> = vec![];
+    let mut numbs: Vec<i32> = vec![];
+    str.chars().for_each(|c| {
+        if c.is_numeric() {
+            numbs.push(c.to_string().parse().unwrap());
+            temp = vec![];
+        } else {
+            temp.push(c);
+            for (i, _) in temp.clone().into_iter().enumerate() {
+                let s: String = temp.split_at(i).1.into_iter().collect();
+                if let Some(a) = to_num(s.as_str()) {
+                    numbs.push(a);
+                    temp = vec![];
+                    break;
+                }
+            }
+        }
+    });
+    return numbs;
+}
+
+fn to_num(str: &str) -> Option<i32> {
+    match str {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None,
+    }
 }
