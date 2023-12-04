a087a437a442dbeda6d2e4972200dfd18eae4024
Jonatan Kruse <jonatankruse@hotmail.com>
2023-12-04 07:45:04 +0100

    Completed day4

--- day4/src/star1.rs
+++ day4/src/star1.rs
@@ -1,5 +1,19 @@
 pub fn star1(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+    let numbs: Vec<(Vec<usize>, Vec<usize>)> = input.lines().map(parse_input).collect();
+    numbs.iter().map(get_points).sum()
+}
+
+fn parse_input(line: &str) -> (Vec<usize>, Vec<usize>) {
+    let line2 = line.split(':').skip(1).next().unwrap();
+    let mut it = line2.split('|');
+    let win_numbs: Vec<usize> = it.next().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let my_numbs: Vec<usize> = it.next().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    return (win_numbs, my_numbs);
+}
+
+fn get_points(numbs: &(Vec<usize>, Vec<usize>)) -> usize {
+    let winnings: Vec<&usize> = numbs.0.iter().filter_map(|num| numbs.1.iter().find(|n| &num == n)).collect();
+    if winnings.len() == 0 { 0 } else {(2 as usize).pow((winnings.len() - 1) as u32)}
 }
 
 #[cfg(test)]
@@ -10,7 +24,7 @@ mod test_star1 {
     fn it_works() {
         let test_input = include_str!("./t1");
         let result = star1(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 13)
     }
 
 }
--- day4/src/star2.rs
+++ day4/src/star2.rs
@@ -1,5 +1,40 @@
 pub fn star2(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+    let mut cards: Vec<Card> = input.lines().map(parse_card).collect();
+    for i in 0..cards.len() {
+        add_cards(i, &mut cards);
+    }
+    dbg!(cards.clone());
+    cards.iter().map(|c| c.copies).sum()
+}
+
+fn parse_card(line: &str) -> Card {
+    let line2 = line.split(':').skip(1).next().unwrap();
+    let mut it2 = line2.split('|');
+    let win_numbs: Vec<usize> = it2.next().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let my_numbs: Vec<usize> = it2.next().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    Card {
+        copies: 1,
+        numbers: (win_numbs, my_numbs)
+    }
+}
+
+fn add_cards(c_index: usize, cards: &mut Vec<Card>) {
+    let card = &cards[c_index];
+    let copies = card.copies.clone();
+    let winnings = card.numbers.0.iter().filter_map(|num| card.numbers.1.iter().find(|n| &num == n)).collect::<Vec<_>>().len();
+    if winnings > 0 {
+        for i in 1..=winnings {
+            let change = &mut cards[c_index + i];
+            change.copies += copies;
+        }
+    }
+    
+}
+
+#[derive(Debug, Clone)]
+struct Card {
+    copies: usize,
+    numbers: (Vec<usize>, Vec<usize>),
 }
 
 #[cfg(test)]
@@ -10,7 +45,7 @@ mod test_star2 {
     fn it_works() {
         let test_input = include_str!("./t2");
         let result = star2(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 30)
     }
 }
 
