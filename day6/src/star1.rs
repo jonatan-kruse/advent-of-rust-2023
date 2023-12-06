935acf401a661ac7e0942065d9ea7fe09668107f
Jonatan Kruse <jonatankruse@hotmail.com>
2023-12-06 14:39:33 +0100

    Completed day6

--- day6/src/star1.rs
+++ day6/src/star1.rs
@@ -1,5 +1,17 @@
+use std::{iter::zip, usize};
+
 pub fn star1(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+    let mut lines = input.lines().map(|l| l.split_whitespace().skip(1).map(|num| num.parse::<usize>().unwrap()));
+    let races: Vec<(usize, usize)> = zip(lines.next().unwrap(), lines.next().unwrap()).collect();
+    races.iter().map(get_amount_of_wins).fold(1, |acc, num| acc * num)
+}
+
+fn get_amount_of_wins((time, dist): &(usize, usize)) -> usize {
+    let variations = (0.try_into().unwrap()..time.to_owned()).map(|speed| {
+        let travel_time = time - speed;
+        travel_time * speed
+    });
+    variations.filter(|t| t > dist).count()
 }
 
 #[cfg(test)]
@@ -10,7 +22,7 @@ mod test_star1 {
     fn it_works() {
         let test_input = include_str!("./t1");
         let result = star1(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 288)
     }
 
 }
--- day6/src/star2.rs
+++ day6/src/star2.rs
@@ -1,5 +1,15 @@
 pub fn star2(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+    let mut lines = input.lines().map(|l| l.replace(" ", "").split(":").skip(1).next().unwrap().parse::<usize>().unwrap());
+    let race: (usize, usize) = (lines.next().unwrap(), lines.next().unwrap());
+    get_amount_of_wins(&race)
+}
+
+fn get_amount_of_wins((time, dist): &(usize, usize)) -> usize {
+    let variations = (0.try_into().unwrap()..time.to_owned()).map(|speed| {
+        let travel_time = time - speed;
+        travel_time * speed
+    });
+    variations.filter(|t| t > dist).count()
 }
 
 #[cfg(test)]
@@ -10,7 +20,7 @@ mod test_star2 {
     fn it_works() {
         let test_input = include_str!("./t2");
         let result = star2(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 71503)
     }
 }
 
