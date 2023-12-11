bdc016f4a45d6de67872b8f3ef6bd11bee66018f
Jonatan Kruse <jonatankruse@hotmail.com>
2023-12-11 08:11:48 +0100

    Completed day11

--- day11/src/star1.rs
+++ day11/src/star1.rs
@@ -1,5 +1,34 @@
-pub fn star1(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+use std::collections::HashSet;
+
+pub fn star1(input: &str) -> i32 {
+    let raw_gid = input.lines().map(|l| l.chars().collect::<Vec<_>>());
+    let mut grid = vec![];
+    for chars in raw_gid {
+        grid.push(chars.clone());
+        if chars.iter().all(|c| c == &'.') {
+            grid.push(chars.clone())
+        }
+    }
+    let transposed = (0..grid[0].len())
+        .map(|col| (0..grid.len()).map(|row| grid[row][col]).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let mut expanded_grid = vec![];
+    for chars in transposed.clone() {
+        expanded_grid.push(chars.clone());
+        if chars.iter().all(|c| c == &'.') {
+            expanded_grid.push(chars.clone())
+        }
+    }
+    let mut set:HashSet<(i32, i32)> = HashSet::new();
+    expanded_grid.iter().enumerate().for_each(|(i, chars)| chars.iter().enumerate().for_each(|(j, c)| if c == &'#' {set.insert((i.try_into().unwrap(), j.try_into().unwrap()));}));
+    let mut distance = 0;
+    for from in set.clone() {
+        set.remove(&from);
+        for to in set.clone() {
+            distance += (from.0 - to.0).abs() + (from.1 - to.1).abs();
+        }
+    }
+    distance
 }
 
 #[cfg(test)]
@@ -10,7 +39,6 @@ mod test_star1 {
     fn it_works() {
         let test_input = include_str!("./t1");
         let result = star1(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 374)
     }
-
 }
--- day11/src/star2.rs
+++ day11/src/star2.rs
@@ -1,5 +1,63 @@
-pub fn star2(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+use std::collections::HashSet;
+
+pub fn star2(input: &str) -> i64 {
+    let raw_gid = input.lines().map(|l| l.chars().collect::<Vec<_>>());
+    let mut grid = vec![];
+    for chars in raw_gid {
+        if chars.iter().all(|c| c == &'.') {
+            grid.push((0..chars.len()).map(|_| '▒').collect());
+        } else {
+            grid.push(chars.clone());
+        }
+    }
+    let transposed = (0..grid[0].len())
+        .map(|col| {
+            (0..grid.len())
+                .map(|row| grid[row][col])
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let mut expanded_grid = vec![];
+    for chars in transposed.clone() {
+        if chars.iter().all(|c| c == &'.' || c == &'▒') {
+            expanded_grid.push((0..chars.len()).map(|_| '▒').collect());
+        } else {
+            expanded_grid.push(chars.clone());
+        }
+    }
+    let mut set: HashSet<(i64, i64)> = HashSet::new();
+    let mut x_expand = 0;
+    let mut y_expand = 0;
+    for (i, chars) in expanded_grid.iter().enumerate() {
+        if chars.iter().all(|c| c == &'▒') {
+            y_expand += 1;
+        }
+        for (j, c) in chars.iter().enumerate() {
+            print!("{c}");
+            if c == &'▒' {
+                x_expand += 1
+            }
+            if c == &'#' {
+                let ii64: i64 = i.try_into().unwrap();
+                let ji64: i64 = j.try_into().unwrap();
+                set.insert((
+                    ii64 + y_expand * 1000000 - y_expand,
+                    ji64 + x_expand * 1000000 - x_expand,
+                ));
+            }
+        }
+        x_expand = 0;
+
+        println!();
+    }
+    let mut distance = 0;
+    for from in set.clone() {
+        set.remove(&from);
+        for to in set.clone() {
+            distance += (from.0 - to.0).abs() + (from.1 - to.1).abs();
+        }
+    }
+    distance
 }
 
 #[cfg(test)]
@@ -13,4 +71,3 @@ mod test_star2 {
         assert_eq!(result, 0)
     }
 }
-
