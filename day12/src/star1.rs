26ff05585561c8d9342d00fab93c8b76db82e8c5
Jonatan Kruse <jonatankruse@hotmail.com>
2023-12-13 14:04:04 +0100

    Complete day12

--- day12/src/star1.rs
+++ day12/src/star1.rs
@@ -1,5 +1,66 @@
 pub fn star1(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+    let list = input.lines().map(|l| {
+        let (text, numbs) = l.split_once(' ').unwrap();
+        (text, numbs.split(',').map(|n| n.parse().unwrap()).collect())
+    });
+    list.map(|(t, n)| combinations(n, t.len()).filter(|c| is_valid(c, t)).count())
+        .sum()
+}
+
+fn is_valid(test: &str, pattern: &str) -> bool {
+    let boo = test.chars()
+    .zip(pattern.chars())
+    .all(|(tc, pc)| pc == '?' || tc == pc);
+    boo
+}
+
+fn combinations(numbs: Vec<usize>, length: usize) -> impl Iterator<Item = String> {
+    let combs = get_combs(numbs, length);
+    combs.into_iter().map(|c| c.into_iter().collect())
+}
+
+fn get_combs(numbs: Vec<usize>, length: usize) -> Vec<Vec<char>> {
+    if length == 0 {
+        return vec![vec![]];
+    }
+     if numbs.is_empty() {
+        return vec![vec!['.'; length]];
+    }
+
+    if numbs.iter().sum::<usize>() + numbs.len() - 1 > length {
+        return vec![];
+    }
+
+    if numbs.len() == 1 && numbs[0] == length {
+        return vec![vec!['#'; length]];
+    }
+
+    let mut result = Vec::new();
+
+    // Generate combinations starting with '#'
+    if let Some((first, rest)) = numbs.split_first() {
+        if *first < length {
+            let mut sub_combs = get_combs(rest.to_vec(), length - first - 1);
+            for sub_comb in &mut sub_combs {
+                let mut new_comb = vec!['#'; *first];
+                new_comb.push('.'); // Ensure separation
+                new_comb.append(sub_comb);
+                result.push(new_comb);
+            }
+        }
+    }
+
+    // Generate combinations starting with '.'
+    if length > 1 {
+        let mut sub_combs = get_combs(numbs.clone(), length - 1);
+        for sub_comb in sub_combs.iter_mut() {
+            let mut new_comb = vec!['.'];
+            new_comb.append(sub_comb);
+            result.push(new_comb);
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -10,7 +71,7 @@ mod test_star1 {
     fn it_works() {
         let test_input = include_str!("./t1");
         let result = star1(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 21)
     }
 
 }
--- day12/src/star2.rs
+++ day12/src/star2.rs
@@ -1,5 +1,97 @@
+use std::collections::HashMap;
+
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
 pub fn star2(input: &str) -> usize {
-    input.lines().collect::<Vec<&str>>().len()
+    let list = input
+        .lines()
+        .map(|l| {
+            let (text, numbs) = l.split_once(' ').unwrap();
+            let numbs: Vec<usize> = numbs.split(',').map(|n| n.parse().unwrap()).collect();
+            let repeated_text = (0..5).map(|_| text).collect::<Vec<&str>>().join("?");
+            (repeated_text, numbs.repeat(5))
+        })
+        .collect::<Vec<_>>();
+
+    // let list = input.lines().map(|l| {
+    //     let (text, numbs) = l.split_once(' ').unwrap();
+    //     (text, numbs.split(',').map(|n| n.parse().unwrap()).collect::<Vec<_>>())
+    // }).collect::<Vec<_>>();
+    list.par_iter()
+        .map(|(t, n)| combinations(n.to_vec(), &t))
+        .sum()
+}
+
+fn combinations(numbs: Vec<usize>, pattern: &str) -> usize {
+    let char_slice: Vec<char> = pattern.chars().collect();
+    let mut map = HashMap::new();
+    let combs = get_combs(&numbs, &char_slice, &mut map);
+    println!("combs: {}", combs);
+    combs
+}
+
+fn get_combs(
+    numbs: &[usize],
+    pattern: &[char],
+    map: &mut HashMap<(Vec<usize>, Vec<char>), usize>,
+) -> usize {
+    if numbs.is_empty() && pattern.is_empty() {
+        return 1;
+    }
+    if let Some(num) = map.get(&(numbs.to_vec(), pattern.to_vec())) {
+        return *num;
+    }
+    if pattern.is_empty() && !numbs.is_empty() {
+        return 0;
+    }
+
+
+    let mut result = 0;
+
+    if let Some((first, rest)) = numbs.split_first() {
+        if number_fits_pattern(*first, &pattern) {
+            if *first + 1 <= pattern.len() {
+                result += get_combs(&rest.to_vec(), &pattern[(*first + 1)..], map);
+            }
+            if *first == pattern.len() {
+                result += get_combs(&rest.to_vec(), &[], map);
+            }
+        }
+    }
+
+    if dot_fits_pattern(pattern) {
+        result += get_combs(numbs, &pattern[1..], map);
+    }
+
+    map.insert((numbs.to_vec(), pattern.to_vec()), result);
+
+    result
+}
+
+fn dot_fits_pattern(pattern: &[char]) -> bool {
+    if pattern[0] == '#' {
+        return false;
+    }
+    true
+}
+
+fn number_fits_pattern(num: usize, pattern: &[char]) -> bool {
+    if num > pattern.len() {
+        return false;
+    }
+    let mut count = 0;
+    for &c in pattern.iter().take(num) {
+        if c == '.' {
+            return false;
+        }
+        if c == '?' || c == '#' {
+            count += 1;
+        }
+    }
+    if pattern.len() > num && pattern[num] == '#' {
+        return false;
+    }
+    count >= num
 }
 
 #[cfg(test)]
@@ -10,7 +102,8 @@ mod test_star2 {
     fn it_works() {
         let test_input = include_str!("./t2");
         let result = star2(test_input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 525152)
     }
 }
-
+// 166239150459353 to big
+// 160500973317706
